2fca1e9b3ad9525fd5c126ea55ab365cd8e79868
Okeybukks <achebepeter94@gmail.com>
2024-03-13 11:45:33 +0000

    edit terraform provider

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,9 +1,16 @@
 terraform {
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = "5.31.0"
+      source = "hashicorp/aws"
+      version = "5.40.0"
     }
+  }
+
+  backend "s3" {
+    bucket         = "achebeh-bucket"
+    key            = "./terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform"
   }
 }
 
