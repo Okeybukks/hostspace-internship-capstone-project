31cac9fc5135a4a47ce2288241f8ff51dd2f3165
Okeybukks <achebepeter94@gmail.com>
2024-03-13 13:19:36 +0000

    edit build file

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,14 @@
-resource "aws_ecr_repository" "foo" {
-  name                 = "hostspace"
+resource "aws_ecr_repository" "backend" {
+  name                 = "kanban-backend"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+resource "aws_ecr_repository" "frontend" {
+  name                 = "kanban-frontend"
   image_tag_mutability = "MUTABLE"
 
   image_scanning_configuration {
