ffd60e8dae71233329496935e9c13f92323119af
Okeybukks <achebepeter94@gmail.com>
2024-03-14 00:28:10 +0000

    test

---
+++ kubernetes_cluster/awscli_eksctl_installation_script.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# awscli installation
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+
+# eksctl installation
+ARCH=amd64
+PLATFORM=$(uname -s)_$ARCH
+curl -sLO "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_$PLATFORM.tar.gz"
+tar -xzf eksctl_$PLATFORM.tar.gz -C /tmp && rm eksctl_$PLATFORM.tar.gz
+sudo mv /tmp/eksctl /usr/local/bin
