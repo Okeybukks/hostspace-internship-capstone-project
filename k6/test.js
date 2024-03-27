3e2b43c4aea1f5a3d21bd7dfbf56481f7cbc6807
Okechukwu Achebe <achebepeter94@gmail.com>
2024-03-27 01:03:03 +0100

    test

--- k6/test.js
+++ k6/test.js
@@ -5,17 +5,16 @@
 import { check, group, sleep } from 'k6'
 import http from 'k6/http'
 
-const TARGET_URL = __ENV.TEST_TARGET || 'https://example.net'
+const TARGET_URL = __ENV.TEST_TARGET || 'https://httpbin.test.k6.io/'
 const RAMP_TIME = __ENV.RAMP_TIME || '5m'
 const RUN_TIME = __ENV.RUN_TIME || '10m'
-const USER_COUNT = __ENV.USER_COUNT || 50
+const USER_COUNT = __ENV.USER_COUNT || 30
 const SLEEP = __ENV.SLEEP || 0
 
 // Very simple ramp up from zero to VUS_MAX over RAMP_TIME, then runs for further RUN_TIME
 export let options = {
   stages: [
     { duration: RAMP_TIME, target: USER_COUNT },
-    { duration: RUN_TIME, target: USER_COUNT },
     { duration: RUN_TIME, target: USER_COUNT },
   ],
 }
