3143f36617a3eed419cf607000fd59adb21e4e8e
Okechukwu Achebe <achebepeter94@gmail.com>
2024-04-13 00:08:54 +0100

    create branch for submission

--- kubeseal/kubeseal_installer.sh
+++ kubeseal/kubeseal_installer.sh
@@ -5,7 +5,3 @@ KUBESEAL_VERSION='0.23.0'
 wget "https://github.com/bitnami-labs/sealed-secrets/releases/download/v${KUBESEAL_VERSION:?}/kubeseal-${KUBESEAL_VERSION:?}-linux-amd64.tar.gz"
 tar -xvzf kubeseal-${KUBESEAL_VERSION:?}-linux-amd64.tar.gz kubeseal
 sudo install -m 755 kubeseal /usr/local/bin/kubeseal
-
-# Sealed Secrets Controller installation
-helm repo add sealed-secrets https://bitnami-labs.github.io/sealed-secrets
-helm install sealed-secrets -n kube-system --set-string fullnameOverride=sealed-secrets-controller sealed-secrets/sealed-secrets
