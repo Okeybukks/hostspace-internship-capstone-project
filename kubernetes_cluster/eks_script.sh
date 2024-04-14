cf13d6d718e947e111cb914fe8a41b8c57cb3515
Okechukwu Achebe <achebepeter94@gmail.com>
2024-04-14 05:00:44 +0100

    test

--- kubernetes_cluster/eks_script.sh
+++ kubernetes_cluster/eks_script.sh
@@ -78,3 +78,5 @@ helm upgrade --install karpenter oci://public.ecr.aws/karpenter/karpenter --vers
   --set controller.resources.limits.cpu=1 \
   --set controller.resources.limits.memory=1Gi \
   --wait
+
+  
